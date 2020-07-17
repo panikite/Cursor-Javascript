01d1ddadbeebf07c3e4d7dd10ea3add517a9a7ec
panikite <ne.isnuje@gmail.com>
2020-07-17 14:02:15 +0300

    add checking for correct input; determination of the max and min value

--- hw_2/index.js
+++ hw_2/index.js
@@ -1,23 +1,41 @@
-const N = parseInt(prompt("Введіть число №1:"));
-const M = parseInt(prompt("Введіть число №2:"));
+let N,
+    M;
+
+N = parseInt(prompt("Введіть число №1:"));
+while (isNaN(N)) {
+    N = parseInt(prompt("Ввести потрібно лише ціле число:"));
+}
+
+M = parseInt(prompt("Введіть число №2:"));
+while (isNaN(M)) {
+    M = parseInt(prompt("Ввести потрібно лише ціле число:"));
+}
+
 const missEven = confirm("Пропускати парні числа?");
 let total = 0;
+
 console.log(
 `Число #1: ${N};
 Число #2: ${M};
 Пропускати парні числа?: ${missEven}`);
 
-if(typeof(N)!=="NaN" && typeof(M)!=="NaN") {
 
-        for(let i = N; i <=M; i++) {
-            if(missEven && i%2==0) {
-                continue;
-            }
-            total+=i;
+const max = Math.max(M,N);
+const min = Math.min(M,N);
+console.log(max,min);
+
+    for(let i = min; i <=max; i++) {
+        if(missEven && i%2==0) {
+            continue;
         }
+        total+=i;
     }
 
 console.log(`Сумма: ${total}`);
+
+
+
+
 
 
 
