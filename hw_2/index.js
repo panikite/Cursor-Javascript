94d6612397264122d1160fd8859d5218b663aaf5
panikite <ne.isnuje@gmail.com>
2020-07-22 12:28:44 +0300

    change variables names, add check for empty string input

--- hw_2/index.js
+++ hw_2/index.js
@@ -1,27 +1,27 @@
-let N,
-    M;
+let number_1,
+    number_2;
 
-N = +(prompt("Введіть число №1:"));
-while (isNaN(N)) {
-    N = +(prompt("Ввести потрібно лише ціле число:"));
+number_1 = +(prompt("Введіть число №1:"));
+while (!number_1 || isNaN(number_1)) {
+    number_1 = +(prompt("Ввести потрібно лише ціле число:"));
 }
 
-M = +(prompt("Введіть число №2:"));
-while (isNaN(M)) {
-    M = +(prompt("Ввести потрібно лише ціле число:"));
+number_2 = +(prompt("Введіть число №2:"));
+while (!number_2 || isNaN(number_2)) {
+    number_2 = +(prompt("Ввести потрібно лише ціле число:"));
 }
 
 const missEven = confirm("Пропускати парні числа?");
 let total = 0;
 
 console.log(
-`Число #1: ${N};
-Число #2: ${M};
+`Число #1: ${number_1};
+Число #2: ${number_2};
 Пропускати парні числа?: ${missEven}`);
 
 
-const max = Math.max(M,N);
-const min = Math.min(M,N);
+const max = Math.max(number_1,number_2);
+const min = Math.min(number_1,number_2);
 
     for(let i = min; i <=max; i++) {
         if(missEven && i%2==0) {
